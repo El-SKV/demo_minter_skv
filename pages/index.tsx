725cd8a965bff55c8f46348c9ed798265a66f9be
SenDevSei <kevin.marous@gmail.com>
2022-11-26 00:33:48 +0100

    update nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
   return (
     <MainLayout>
       <HeaderMenu>
-        <HeaderMenuButtons enabled={['auth', 'mint', 'about']} />
+        <HeaderMenuButtons enabled={['auth', 'mint']} />
       </HeaderMenu>
       <Box
         display="flex"
